c1313be02d41c4ef9650a2b75ac8172d617cf09a
Mardoniosc <mardonio@live.com>
2020-09-28 15:59:32 -0300

    :v: Tratando msg de erro

--- src/main/java/br/com/mardonio/resources/Tarefas.java
+++ src/main/java/br/com/mardonio/resources/Tarefas.java
@@ -1,7 +1,5 @@
 package br.com.mardonio.resources;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;

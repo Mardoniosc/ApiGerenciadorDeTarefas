ed0ad9b911059105b2744dd6634c89ecac566f68
Mardoniosc <mardonio@live.com>
2020-09-28 13:51:08 -0300

    Domain funcionando na classe Tarefa

--- src/main/java/br/com/mardonio/resources/Tarefas.java
+++ src/main/java/br/com/mardonio/resources/Tarefas.java
@@ -1,15 +1,28 @@
 package br.com.mardonio.resources;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mardonio.domain.Tarefa;
 
 @RestController
 @RequestMapping("/tarefas")
 public class Tarefas {
 	
 	@GetMapping
-	public String funcionando() {
-		return "Tarefas";
+	public List<Tarefa> listar() {
+		Date agora = new Date(System.currentTimeMillis());
+		Tarefa t1 = new Tarefa(1, "Algo", 1, "Fazer", agora, agora, agora, agora, 0);
+		Tarefa t2 = new Tarefa(2, "Algo2", 1, "Fazer 2", agora, agora, agora, agora, 0);
+		
+		List<Tarefa> lista = new ArrayList<>();
+		lista.add(t1);
+		lista.add(t2);
+		return lista;
 	}
 }
